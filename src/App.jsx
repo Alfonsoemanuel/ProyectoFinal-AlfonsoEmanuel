73404e9cad0ce1f168aecf525b08c1a18a4ab7bf
Emanuel Alfonso <emaalfonso95@gmail.com>
2024-02-09 22:13:01 -0300

    modifique app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import NavBar from './Components/NavBar/NavBar'
-import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
 import { BrowserRouter, Routes , Route} from 'react-router-dom'
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer'
-
 import { CartProvider } from './Contenxt/CartContenxt'
-
+import { NotificationProvider } from './Components/notification/NotificationService'
 function App() {
 
   return (
     <>
     <BrowserRouter>
     <NavBar/>
+    <NotificationProvider>
+    <CartProvider>
       <Routes>
         <Route path='/' element={<ItemListContainer greeting={'Listado de todos los productos'}/>} />
         <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Listado de los productos filtrados'}/>} />
         <Route path='/detail/:id' element={<ItemDetailContainer/>} />
         <Route path='/*' element={<h2>No existe sitio</h2>} />
       </Routes>
+      </CartProvider>
+      </NotificationProvider>
     </BrowserRouter>
     </>
   )
